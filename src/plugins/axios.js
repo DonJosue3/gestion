4f22823d857eeadc1d9c479197bd6a7b3e49236e
DonJosue3 <ishakaryayodonjosue@gmail.com>
2024-09-03 21:34:47 +0200

    axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-	baseURL: "https://present-3.onrender.com/urusoro/"
+	baseURL: "http:///localhost:8000/urusoro/"
 })
 
 export default axiosInstance;
